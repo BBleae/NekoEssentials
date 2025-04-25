9100241c6e740107693e48432046423fd299529d
春河晴 <bbleae@outlook.com>
2025-04-25 13:01:45 +0900

    install fabric mod template

---
+++ src/client/kotlin/net/nekocraft/NekoEssentialsDataGenerator.kt
@@ -0,0 +1,9 @@
+package net.nekocraft
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object NekoEssentialsDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
